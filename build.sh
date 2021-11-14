daa880a7dde41992c5de1ffd7197a634ad73d389
SethThyer <seththyer23@gmail.com>
2021-11-14 20:57:59 +1100

    core functionality

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
 gcc ./scroll.c
+mv ./a.out scroll
+cp ./scroll /usr/local/bin
+
+# If saved dir is changed then you must change the two times it's referenced in scroll.c
+touch /home/seth/tmp/saved
--- scroll.c
+++ scroll.c
@@ -1,46 +1,78 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <unistd.h>  //dis
 
 int WriteToSave(char buf[], int pos, int isnew) {
-    int maxlength = 10;
+    int maxlength = 29; // Change this to your liking
     
-    FILE* savedfp = fopen("saved", "w");
+    if (isnew == 1)
+        pos = 0;
+
+    // size of buffer 
+    size_t mls;
+
+    if ((sizeof(char) * maxlength) > strlen(buf)) {
+        mls = (sizeof(char) * maxlength);
+    }
+    else {
+        mls = strlen(buf);
+    }
+
+
+    char *altbuf = malloc(mls);
+    
+    for (int it=0; it < mls; it++) {
+        altbuf[it] = ' ';
+
+        if (it < strlen(buf)) {
+            altbuf[it] = buf[it];
+        }
+    }
+    int full = strlen(altbuf);
+
+
+    // Scrolling happens here    
+    char tmp[full * 2];
+    int offset = full - pos;
+    
+    for (int i=0; i < full; i++) 
+        tmp[i + pos] = altbuf[i];
+    
+    for (int in=0; in < pos; in++)
+        tmp[in] = altbuf[in + offset];
+    
+    for (int it=maxlength + 1; it < strlen(tmp); it++) {
+        tmp[it] = '\0';
+    }
+    
+    
+    //printf("|");
+    printf("%s\n", tmp);
+    //printf("|");
+    
+    pos = pos + 1;
+    printf("Len:%i\n", full);
+    if (pos > full) 
+        pos = 0;
+
+    
+    // DEUBUG 
+    printf("POS:%i\n", pos);
+    
+
+    // Write new position to file
+    FILE* savedfp = fopen("/home/seth/tmp/saved", "w");
     if(savedfp == NULL) {
         printf("Save file not found\n");
         return -1;
     }
 
-    if (isnew == 1)
-        pos = 0;
+    char newl = '\n';
+    fwrite(buf, strlen(buf), 1, savedfp); 
+    fwrite(&newl, 1, 1, savedfp);
+    putw(pos, savedfp);
     
-    char tmp[512];
-    int offset = (int)strlen(buf) - 1 - pos;
-    
-    for (int i=0; i < offset; i++) 
-        tmp[i + pos] = buf[i];
-    
-    for (int in=0; in < pos; in++)
-        tmp[in] = buf[in + offset];
-    
-    
-    printf("|%s", tmp);
-    printf("|");
-
-    pos++;
-    if (pos >= (int)strlen(buf) - 1) 
-        pos = 0;
-    
-    char posstr[100];
-    sprintf(posstr, "%d", pos);
-    
-    // DEUBUG 
-    //printf("%s", posstr);
-    
-    fwrite(buf, strlen(buf), 1, savedfp);  
-    fwrite(&posstr, strlen(posstr), 1, savedfp);  
-    
+    free(altbuf);
     fclose(savedfp);
     return 0;
 }
@@ -48,35 +80,40 @@ int WriteToSave(char buf[], int pos, int isnew) {
 
 int main (void) {
     
-    int maxLength = 100;
-    char delim[] = "||  \n";
-
-    char buf[maxLength + 1 + strlen(delim)];
+    int MAbuflen = 100;
+    char delim[] = "# ";
+    
+    char buf[MAbuflen + 1 + strlen(delim)];
     int size = sizeof(buf);
     fgets(buf, size, stdin); // Put input into buff
     size_t buflen = strlen(buf);
-
+    
     
     strcat(buf, delim);
     buf[buflen - 1] = ' ';
 
-    FILE* savedfp = fopen("saved", "r");
+
+    // Read saved position and string data from save file
+    FILE* savedfp = fopen("/home/seth/tmp/saved", "r");
     if (savedfp == NULL) {
         printf("Save file not found\n");
         return -1;
     }
     
     char savedstr[512];
-    char savedstrpos[512], *endp;
     fgets(savedstr, 512, savedfp);    // Line 1 of saved file into savedstr
-    fgets(savedstrpos, 512, savedfp); // Line 2 of saved file into savedpos
+    int pos = getw(savedfp);          // Line 2 of saved file into savedpos
     
-    int pos = strtol(savedstrpos, &endp, 0);
+    
+    // If the string to be scrolled is changed then reset scroll position
+    savedstr[strlen(savedstr) - 1] = '\0';
+
     if (strcmp(buf, savedstr) == 0) {
         WriteToSave(buf, pos, 0);
         fclose (savedfp);
         return 0;
     }
+
     WriteToSave(buf, pos, 1);
     fclose (savedfp);
     
